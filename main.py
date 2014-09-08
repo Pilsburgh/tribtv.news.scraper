d3e25c9a344253163e1e1cc41237a16e74e289e1
Choomogo <archibald@choomogo.com>
2014-09-08 08:51:41 -0500

    Completed preliminary work to parse M3U8 files.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ STREAMS_URL = 'http://cdn.tribtv.com/ake/embed.html?station=wreg&feed=1&auto=tru
 # Akamia Doesn't like us pulling the m3u8 file when we don't legitimately need it.
 # So we pretend that we are running Android.
 USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (\
-    KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36'
+KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36'
     
 # Matches lines that contain master.m3u8 URIs, station ids, and feed ids.
 # Result: 'wgn[5] = "http://wgntribune-lh.akamaihd.net/z/WGNTribune3_1@192102/master.m3u8";'
@@ -21,11 +21,11 @@ REGEX_M3U8_LINE = re.compile('\w*\[\d\].*master.m3u8";')
 
 # Matches the URI for the m3u8
 # Result: 'http://wgntribune-lh.akamaihd.net/z/WGNTribune3_1@192102/master.m3u8'
-REGEX_M3U8_URI = re.compile('http:\/\/.+m3u8')
+REGEX_M3U8_URL = re.compile('http:\/\/.+m3u8')
 
-# Matches stationId
+# Matches stationName
 # Result: 'wgn'
-REGEX_STATION_ID = re.compile('\w+(?=\[\d\])')
+REGEX_STATION_Name = re.compile('\w+(?=\[\d\])')
 
 # Matches feedName
 # Result: 'WGNTribune3_1'
@@ -35,12 +35,43 @@ REGEX_FEED_NAME = re.compile('[^\/][\w\d_]+(?=\@\d+\/)')
 # Result: '192102'
 REGEX_FEED_ID = re.compile('\d+(?=\/.*m3u8)')
 
+# Matches EXT-X-STREAM-INF line in an m3u8 file.
+# Result: '#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=382000,RESOLUTION=320x180,CODECS="avc1.66.30, mp4a.40.2"'
+REGEX_INF_LINE = re.compile('.+EXT-X-STREAM-INF.+')
+
+# Matches an http URL
+# Result: 'http://wregtribune-lh.akamaihd.net/i/WREGTribunePrimary_1@192110/index_350_av-b.m3u8?sd=10&rebase=on'
+REGEX_HTTP_URL = re.compile('http:\/\/[^\s]+')
+
+# Matches the resolution of the video stream.
+# Result: '320x180'
+REGEX_INF_RESOLUTION = re.compile('[^=]+(?=,CODECS)')
+
+# Matches the bandwidth of the video stream.
+# Result: '382000'
+REGEX_INF_BANDWIDTH = re.compile('[^=]+(?=,RESOLUTION)')
+
+# Matches the codecs of a video stream.
+# Result: 'avc1.66.30, mp4a.40.2'
+REGEX_INF_CODECS = re.compile('[^="]+(?=\")')
+
 # List of stations. Each station is a list consisting of one or more feeds that correspond
 # to that particular station.
-# Suchthat- {'stationId': 'station_code', 'feeds':[{'feedId': '', 'manifestURI': 'manifest.m3u8_URI'}]}
+# Suchthat- {'stationName', 'feedId', 'feedName', 'm3u8URL'}
 stations = []
 
 # streamsReq = urllib2.Request(STREAMS_URL, {}, {'User-Agent': USER_AGENT} )
 # try: urllib2.urlopen(streamsReq)
 # except URLError as e:
 #     print e.reason 
+
+def getM3U8(m3u8URL):
+    m3u8Req = urllib2.Request(m3u8URL, None, {'User-Agent': USER_AGENT})
+    try:
+        response = urllib2.urlopen(m3u8Req)
+        html = response.read()
+        response.close()
+        return html
+    except:
+        return None
+    
