8ce2a0b96069bebc13fe72c3d559ce6ec5268b6e
Choomogo <archibald@choomogo.com>
2014-09-11 15:41:59 -0500

    Feeds properly parse.

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ STREAMS_URL = 'http://cdn.tribtv.com/ake/embed.html?station=wreg&feed=1&auto=tru
 # So we pretend that we are running Android.
 USER_AGENT = 'Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (\
 KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36'
+
+# If True, extra output will be displayed
+VERBOSE = True
     
 # Matches lines that contain master.m3u8 URIs, station ids, and feed ids.
 # Result: 'wgn[5] = "http://wgntribune-lh.akamaihd.net/z/WGNTribune3_1@192102/master.m3u8";'
@@ -100,12 +103,13 @@ def getFeedM3U8(m3u8URL):
         html = response.read()
         response.close()
         return html
-    except:
+    except Exception,e:
+        print(e)
         return None
     
 def insertStations(stations):
     for station in stations:
-        success = insertStation(station['stationName'])
+        success = insertStation(station['stationName'])  # @UnusedVariable
 #         print station['stationName'] + ' successful? ' + str(success)
     
 def insertStation(stationName, stationState='', stationCity=''):
@@ -138,8 +142,49 @@ def insertFeed(feedId, stationId, feedName, feedUrl, resolution='', bandwidth=''
             return False
     else:
         return False
+    
+def parseFeed(stationName, m3u8URL):
+    feeds = []
+    try:
+        m3u8 = getFeedM3U8(m3u8URL)
+        infLines = REGEX_INF_LINE.findall(m3u8)
+        httpLines = REGEX_HTTP_URL.findall(m3u8)
+        
+        for x in range(0, len(infLines)):
+            feed = {}
+            feed['feedId'] = REGEX_FEED_ID.search(httpLines[x]).group()
+            feed['stationId'] = _getStationId(stationName)
+            feed['feedName'] = REGEX_FEED_NAME.search(httpLines[x]).group()
+            feed['feedUrl'] = httpLines[x]
+            feed['resolution'] = REGEX_INF_RESOLUTION.search(infLines[x]).group()
+            feed['bandwidth'] = REGEX_INF_BANDWIDTH.search(infLines[x]).group()
+            feed['codecs'] = REGEX_INF_CODECS.search(infLines[x]).group()
+            feeds.append(feed)
+            
+        return feeds
+    except:
+        return None
+    
+def parseFeedTest(stations):
+    matches = (x for x in stations if x['stationName'] == 'WREG')
+    match = None
+    for x in matches:
+        match = x
+        
+    feeds = parseFeed(match['stationName'], match['m3u8URL'])
+    return feeds
 
-def getStationId(stationName):
+def parseFeeds(stations):
+    feeds = []
+    stationIndex = 1
+    for station in stations:
+        feed = parseFeed(station['stationName'], station['m3u8URL'])
+        if feed != None: feeds.append(feed)
+        if VERBOSE: print 'Parsed station %s of %s' % (stationIndex, len(stations))
+        stationIndex += 1
+    return feeds
+
+def _getStationId(stationName):
     ''' Returns the primary key for a given station name. '''
     if db:
         return db.execute('SELECT STATION_ID FROM stations WHERE STATION_NAME=?', [stationName]).fetchone()[0]
@@ -151,5 +196,5 @@ db = connectDB('feeds.sqlite')
 stationLines = getStationLines(STREAMS_URL)
 stations = parseStationLines(stationLines)
 insertStations(stations)
-print getStationId(stations[3]['stationName'])
+feeds = parseFeeds(stations)
 db.close()
